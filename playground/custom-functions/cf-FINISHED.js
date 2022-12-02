b83d22ec6ebf42c27823bf20a0b3f62883462c9f
etran <emily@tran21.com>
2022-12-02 13:14:29 -0700

    Fixing ESLint file

--- playground/custom-functions/cf-FINISHED.js
+++ playground/custom-functions/cf-FINISHED.js
@@ -1,9 +1,9 @@
 // Function Definition
 function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
-  // this is the function body
-  console.log('Running Calculate Bill!!');
-  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
-  return total;
+   // this is the function body
+   console.log("Running Calculate Bill!!");
+   const total = billAmount + billAmount * taxRate + billAmount * tipRate;
+   return total;
 }
 
 // Function Call. Or **Run**
@@ -13,18 +13,18 @@ const wesTaxRate = 0.3;
 
 // Function Definition
 function sayHiTo(firstName) {
-  return `Hello ${firstName}`;
+   return `Hello ${firstName}`;
 }
 
 // const greeting = sayHiTo('Wes');
 // console.log(greeting);
 
 function doctorize(name) {
-  return `Dr. ${name}`;
+   return `Dr. ${name}`;
 }
 
-function yell(name = 'Silly Goose') {
-  return `HEY ${name.toUpperCase()}`;
+function yell(name = "Silly Goose") {
+   return `HEY ${name.toUpperCase()}`;
 }
 
 const myBill4 = calculateBill(100, undefined, 0.2);
--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,8 +1,11 @@
-//function definition
+const bill = 100;
+const taxRate = 0.13;
+
+// function definition
 function calculateBill() {
    const total = 100 * 1.13;
    return total;
 }
 
 const myTotal = calculateBill();
-console.log(`Your total is: $${myTotal}`)
+console.log(`Your total is: $${myTotal}`);
